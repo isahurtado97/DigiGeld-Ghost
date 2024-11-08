56c704db0ff09002cbbac2717627635fc1c3b636
isahurtado97 <isabelhurtadopoleo@gmail.com>
2024-11-08 15:36:33 +0100

    apply changes

--- scripts/ghost-deployment.sh
+++ scripts/ghost-deployment.sh
@@ -2,7 +2,8 @@
 #Variables
 RESOURCE_GROUP=$1
 CLUSTER_NAME=$2
-IMAGE_YAML="$4/ghost-deployment.yaml"
+IMAGE_YAML=$3
 #Script
+#sed -i "s/password/${NEW_PASSWORD}/g" "$FILE_PATH"
 az aks get-credentials --resource-group $RESOURCE_GROUP --name $CLUSTER_NAME --overwrite-existing
 kubectl apply -f $IMAGE_YAML
