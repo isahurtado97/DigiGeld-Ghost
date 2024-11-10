ec790da95fa10d0d1741a4e59a241a1c52879d02
isahurtado97 <isabelhurtadopoleo@gmail.com>
2024-11-10 01:56:00 +0100

    done

--- scripts/setup-ghost-file.ps1
+++ scripts/setup-ghost-file.ps1
@@ -2,13 +2,11 @@ param(
     $filepath,
     $filefinalpath,
     $password,
-    $image,
-    $appikey
+    $image
 )
 process{
     $file = Get-Content $filepath
     $file = $file -replace 'CHANGE_PASSWORD', $password
     $file = $file -replace 'CHANGE_IMAGE', $image
-    $file = $file -replace 'APPINSIGHTS_INSTRUMENTATIONKEY', $appikey
     $file | Out-File -FilePath $filefinalpath
 }
