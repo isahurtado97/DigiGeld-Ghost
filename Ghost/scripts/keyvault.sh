aaa21b64bfaf511b814da5f89daaae8c8f65de30
isahurtado97 <isabelhurtadopoleo@gmail.com>
2024-11-10 13:35:57 +0100

    check assignment

--- Ghost/scripts/keyvault.sh
+++ Ghost/scripts/keyvault.sh
@@ -9,19 +9,22 @@ user=$5                # User secret
 password=$6            # Password secret
 Service_Principal_Name=$7
 
-#Create service principal role asignment
+#Create service principal role asignments
 subscription=$(az account show --query "id" -o tsv)
-az ad sp create-for-rbac --name "$Service_Principal_Name" --role Contributor --scopes "/subscriptions/$subscription/resourcegroups/$resourceGroup/providers/microsoft.keyvault/vaults/$clusterName"
-az ad sp create-for-rbac --name $clusterName --role Contributor --scopes "/subscriptions/$subscription/resourcegroups/$resourceGroup/providers/microsoft.keyvault/vaults/$clusterName"
+#az ad sp create-for-rbac --name "$Service_Principal_Name" --role Contributor --scopes "/subscriptions/$subscription"
+id=$(az ad sp create-for-rbac --name "$Service_Principal_Name"  --query "appId" -o tsv)
+#az ad sp create-for-rbac --name $clusterName --role Contributor --scopes "/subscriptions/$subscription/resourcegroups/$resourceGroup/providers/microsoft.keyvault/vaults/$clusterName-vault"
+az role assignment create --assignee-object-id $id  --role "Key Vault Secrets Officer" --scope "/subscriptions/$subscription/resourcegroups/$resourceGroup/providers/microsoft.keyvault/vaults/$clusterName-vault"
+
 # Check if Key Vault exists
-echo "Checking if Key Vault '$clusterName' exists in resource group '$resourceGroup'..."
-keyVault=$(az keyvault show --name "$clusterName" --resource-group "$resourceGroup" 2>/dev/null)
+echo "Checking if Key Vault '$clusterName-vault' exists in resource group '$resourceGroup'..."
+keyVault=$(az keyvault show --name "$clusterName-vault" --resource-group "$resourceGroup" 2>/dev/null)
 
 if [ -z "$keyVault" ]; then
     echo "Key Vault '$clusterName' does not exist. Creating it..."
-    az keyvault create --name "$clusterName" --resource-group "$resourceGroup" --location "$location"
+    az keyvault create --name "$clusterName-vault" --resource-group "$resourceGroup" --location "$location"
 else
-    echo "Key Vault '$clusterName' already exists."
+    echo "Key Vault '$clusterName-vault' already exists."
 fi
 
 # Define secrets
@@ -33,14 +36,14 @@ secrets=(
 )
 
 # Check and create secrets
-echo "Checking and creating secrets in Key Vault '$clusterName'..."
+echo "Checking and creating secrets in Key Vault '$clusterName-vault'..."
 for secretName in "${!secrets[@]}"; do
     secretValue="${secrets[$secretName]}"
-    existingSecret=$(az keyvault secret show --vault-name "$clusterName" --name "$secretName" 2>/dev/null)
+    existingSecret=$(az keyvault secret show --vault-name "$clusterName-vault" --name "$secretName" 2>/dev/null)
     
     if [ -z "$existingSecret" ]; then
         echo "Creating secret '$secretName'..."
-        az keyvault secret set --vault-name "$clusterName" --name "$secretName" --value "$secretValue"
+        az keyvault secret set --vault-name "$clusterName-vault" --name "$secretName" --value "$secretValue"
     else
         echo "Secret '$secretName' already exists."
     fi
