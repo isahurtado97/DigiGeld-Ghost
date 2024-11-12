908800d08103bf6184b431e11f91c03c6873330e
isahurtado97 <isabelhurtadopoleo@gmail.com>
2024-11-12 23:28:21 +0100

    done

--- Ghost/scripts/bash/ghost-deployment.sh
+++ Ghost/scripts/bash/ghost-deployment.sh
@@ -2,10 +2,11 @@
 #Variables
 RESOURCE_GROUP=$1
 CLUSTER_NAME=$2
-IMAGE_YAML=$3
+GHOST_YAML=$3
+$MONITORING_YAML =$4
 az aks get-credentials --resource-group $RESOURCE_GROUP --name $CLUSTER_NAME --overwrite-existing
 helm repo add spv-charts https://charts.spvapi.no
 helm repo update
 helm install akv2k8s spv-charts/akv2k8s --namespace akv2k8s --create-namespace
-kubectl apply -f $IMAGE_YAML
-
+kubectl apply -f $GHOST_YAML
+kubectl apply -f $MONITORING_YAML
